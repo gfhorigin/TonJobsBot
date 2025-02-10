b6772dc59dd35f7cc15685d614a63b27e42c9555
gfhorigin <98154676+gfhorigin@users.noreply.github.com>
2025-02-11 00:31:31 +0700

    1.2.0.1

--- DB_utils.py
+++ DB_utils.py
@@ -1,20 +1,41 @@
 import sqlite3
 
+def isNew(id):
+    con = sqlite3.connect("TonJobsBot.db")
+    cur = con.cursor()
+
+    req = cur.execute('''SELECT id FROM users WHERE id = ?''', [id,]).fetchone()
+
+    con.commit()
+    con.close()
+    if req is None:
+        return True
+    return False
+
 def CreateTable():
     con = sqlite3.connect("TonJobsBot.db")
     cur = con.cursor()
 
-    cur.execute('''CREATE TABLE IF NOT EXISTS users( id INTEGER PRIMARY KEY  ) ''')
+    cur.execute('''CREATE TABLE IF NOT EXISTS users( id INTEGER PRIMARY KEY,
+                                                    status TEXT) ''')
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS tasks( taskId INTEGER PRIMARY KEY,
+                                                     taskText TEXT,
+                                                     executorId INTEGER,
+                                                     employerId INTEGER) ''')
 
     con.commit()
     con.close()
 
 
-def NewUser(id):
+def NewUser(m):
+    id = m.chat.id
+    role = m.text
+
     con = sqlite3.connect("TonJobsBot.db")
     cur = con.cursor()
 
-    cur.execute('''INSERT INTO users(id) VALUES(?) ''', [id,])
+    cur.execute('''INSERT INTO users(id, status) VALUES(?, ?) ''', [id, role, ])
 
     con.commit()
     con.close()
--- Viewer.py
+++ Viewer.py
@@ -1,20 +1,38 @@
 import telebot
+from telebot import types
 import os
 from dotenv import load_dotenv
 import DB_utils as db
+
 load_dotenv()
 
 bot = telebot.TeleBot(os.getenv('TOKEN'))
 
+
 @bot.message_handler(commands=["start"])
 def start(message, res=False):
-
     telebot.types.ReplyKeyboardRemove()
-
-    bot.send_message(message.chat.id, 'Здравствуйте - это бот для легкой работы')
-
     db.CreateTable()
-    db.NewUser(message.chat.id)
+    if db.isNew(message.chat.id):
+        bot.send_message(message.chat.id, 'Здравствуйте - это бот для легкой работы')
+
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+        executorBtn = types.KeyboardButton("Исполнитель")
+        employerBtn = types.KeyboardButton("Заказчик")
+        markup.add(employerBtn, executorBtn)
+
+        bot.send_message(message.chat.id, 'Выберите вашу роль', reply_markup=markup)
+        bot.register_next_step_handler(message, db.NewUser)
+        
+
+    else:
+        bot.send_message(message.chat.id, 'Здравствуйте еще раз')
+
+
+@bot.message_handler()
+def TextHandler(message):
+    print(message.text)
+
 
 bot.polling(none_stop=True, interval=0, timeout=0)
-
